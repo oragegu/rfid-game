f9ce462ff3e68249d84d37334c886e35e78ba4ee
oragepika <me@dansmonorage.blue>
2023-01-16 17:51:40 +0100

    tidying up and better encapsulation

--- scan.py
+++ scan.py
@@ -17,7 +17,10 @@ def show_first_read_tag(rx):
             receiver.flushInput()
 
 def RSSI(rx):
-    bytes_data = [0x01,0x46,0x46,0x05,0x05,0x0d]
+    bytes_data = [0x01,0x46,0x46,0x02,0x46,0x44, 0x01, ]
+    #SOH <add h> <add l> STX ‘F’ ‘D’ ‘0’ <antenna> <freq 1 h> <freq 1 l>
+    #<freq 2 h> <freq 2 l> <freq 3 h> <freq 3 l> ETX <bcc> CR
+    #
     receiver.write(bytes_data)
     receiver.timeout = 1
     rx = receiver.read(size=39)
