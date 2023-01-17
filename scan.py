7d3f25f2b8e09d34ebb5bc179f443a85c54abb32
oragepika <me@dansmonorage.blue>
2023-01-17 21:25:25 +0100

    RSSI has entered the detection if != condition but the return value is incorrect

--- scan.py
+++ scan.py
@@ -28,14 +28,14 @@ def show_first_read_tag(rx):
     receiver.timeout = 1
     rx = receiver.read(size=39)
     if rx.hex() != '014646023030303030303030303003000d':  # ignore empty response (when no tag scanned)
-            tag_code = str(rx[4:36], 'utf-8')
-            print(tag_code)
-            receiver.write(b'01464606070D')
-            sleep(1)
-            receiver.flushInput()
+        tag_code = str(rx[4:36], 'utf-8')
+        print(tag_code)
+        receiver.write(b'01464606070D')
+        sleep(1)
+        receiver.flushInput()
 
 def RSSI(rx):
-    bytes_data = [0x01,add_h,add_l,0x02,0x46,0x44, 0x01, freq_1_h, freq_1_l, freq_2_h, freq_2_l, freq_3_h, freq_3_l, 0x03, 0x7d, 0x0d]
+    bytes_data = [0x01,add_h,add_l,0x02,0x46,0x44, 0x30, 0x01, freq_1_h, freq_1_l, freq_2_h, freq_2_l, freq_3_h, freq_3_l, 0x03, 0x00, 0x0d]
     # SOH <add h> <add l> STX ‘F’ ‘D’ ‘0’ <antenna> <freq 1 h> <freq 1 l>
     #<freq 2 h> <freq 2 l> <freq 3 h> <freq 3 l> ETX <bcc> CR
     # The frequency to test in MHz in the range 840 – 960
@@ -43,19 +43,21 @@ def RSSI(rx):
     receiver.write(bytes_data)
     receiver.timeout = 1
     rx = receiver.read(size=39)
-    if rx.hex() != '014646023030303030303030303003000d':  # ignore empty response (when no tag scanned)
-            tag_code = str(rx[4:36], 'utf-8')
-            print(tag_code)
-            receiver.write(b'01464606070D')
-            sleep(1)
-            receiver.flushInput()
+    if rx.hex() != '014646023030303030303030303003000d':  # ignore empty response (when no tag scanned)  
+        print(rx)
+        tag_code = str(rx[4:36], 'utf-8')
+        print(tag_code)
+        receiver.write(b'01464606070D')
+        sleep(1)
+        receiver.flushInput()
 
 def main():
    
     while(True):
         rx=0
         sleep(0.5)
-        show_first_read_tag(rx)
+        #show_first_read_tag(rx)
+        RSSI(rx)
 
 main()
 receiver.close()
