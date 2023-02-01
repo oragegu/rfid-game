a6b07654825706ab40b8def125eb5771086649c5
kdobretz <86402002+kdobretz@users.noreply.github.com>
2023-02-01 21:55:12 +0100

    fake hide and seek ok, need 2 remove ugly prints

--- Functions_RFID.py
+++ Functions_RFID.py
@@ -2,7 +2,10 @@
 
 import time
 import math
-import scan2 as scan
+import scan
+import tkinter as tk
+from tkinter import messagebox
+import random
 #début du main
 ans=''        
 welcome_message ="""
@@ -15,13 +18,13 @@ write either:
 def capture():
     tag=''
     while (tag==''):
-        tag=scan.show_first_read_tag()
+        tag=scan.show_first_read_tag() 
     return tag
 
-def evaluate_distance(Pr,Pe,Gr,Ge,freq,eps_r=1,mu_r=1):
+def evaluate_distance(Pr,Pt,Gr,Gt,freq,eps_r=1,mu_r=1):
     lmbda=(3*10**8)/(freq*mu_r*eps_r)
-    R = (lmbda/(4*math.pi)) * math.sqrt((Pe*Ge*Gr)/Pr) 
-    return(R)
+    R = (lmbda/(4*math.pi)) * math.sqrt((Pt*Gt*Gr)/Pr) 
+    return(R/(R+1))
 
 def timer(n:int):
     for i in range(0,n):
@@ -61,17 +64,50 @@ def calibrate(nbplayers:int):
                 
     return players
 
+def get_BB_settings():
+    Pt = 1.58
+    Gr = 1.64
+    Gt = 7.07
+    freq = 866.5*10**6
+    return Pt,Gr,Gt,freq
+'''
 def hide_and_seek(players):
     print("press a key when ready to start the timer")
     b = input("$>")
     timer(3)
-    caught=['','']
-    while (len(caught)<len(players)):
-        tag=capture() #must include a wait/while system
-        if (evaluate_distance()<1): #complete here    
-            caught.append(tag)
-            #remove tag from sought after tags (players)
-
+    caught=[]
+    Pt,Gr,Gt,freq= get_BB_settings()
+    while (len(caught)<len(players)-1):
+        print("tag")
+        tag = capture()
+        print("tag passed")
+        Pr = generate_random_Pr()
+        print(Pr + " dB")
+        print(evaluate_distance(Pr,Pt,Gr,Gt,freq)+" metres")
+        if (evaluate_distance(Pr,Pt,Gr,Gt,freq)<1):    
+            caught.append(tag) 
+            print(caught)
+    return caught
+'''
+def hide_and_seek(players):
+    print("press a key when ready to start the timer")
+    b = input("$>")
+    timer(3)
+    caught=[]
+    while (len(caught)<(len(players)-1)):
+        tag=capture()
+        Pr = generate_random_Pr()
+        Pt = 1.58
+        Gr = 1.64
+        Gt = 7.07
+        freq = 866.6*10**6
+        if (evaluate_distance(Pr,Pt,Gr,Gt,freq)<1): #complete here    
+            if(tag not in caught):
+                caught.append(tag)
+            print(str(evaluate_distance(Pr,Pt,Gr,Gt,freq)) + " metres")
+    print(caught)
+    return(caught)
+            
 def one_two_three_sun(players):
     print("press a key when ready to start the timer")
     b = input("$>")
@@ -84,9 +120,50 @@ def one_two_three_sun(players):
         tag=capture() #all captures get caught
         caught.append(tag)
 
+def on_calibrate():
+    nbplayers = int(players_entry.get())
+    players = calibrate(nbplayers)
+    messagebox.showinfo("Info", "Players have been calibrated.")
+    return players
+
+def generate_random_Pr():
+    return random.uniform(8.91*10**-2, 8.91*10**-6)
+
+def on_hide_and_seek():
+    caught = hide_and_seek(players)
+    messagebox.showinfo("Info", "The game of hide and seek has finished.")
+    return caught
+
+def on_one_two_three_sun():
+    one_two_three_sun(players)
+    messagebox.showinfo("Info", "The game of one, two, three sun has finished.")
+
+##game launch
 print(welcome_message)
 players=load_players()
 print(players)
+root = tk.Tk()
+root.title("RFID Game Console")
+
+players_label = tk.Label(root, text="Number of players:")
+players_label.grid(row=0, column=0, padx=10, pady=10)
+
+players_entry = tk.Entry(root)
+players_entry.grid(row=0, column=1, padx=10, pady=10)
+
+calibrate_button = tk.Button(root, text="Calibrate", command=on_calibrate)
+calibrate_button.grid(row=1, column=0, padx=10, pady=10)
+
+hide_and_seek_button = tk.Button(root, text="Hide and Seek", command=on_hide_and_seek)
+hide_and_seek_button.grid(row=1, column=1, padx=10, pady=10)
+
+one_two_three_sun_button = tk.Button(root, text="One, Two, Three Sun", command=on_one_two_three_sun)
+one_two_three_sun_button.grid(row=2, column=0, padx=10, pady=10, columnspan=2)
+
+root.mainloop()
+
+'''
+#reactivate this code if we can't use windows
 while ans != 'exit':
     a = input("$>")
     a = a.split(" ")
@@ -103,3 +180,4 @@ while ans != 'exit':
     if ans == 'soleil':
         one_two_three_sun(players)
     print(welcome_message)
+'''
